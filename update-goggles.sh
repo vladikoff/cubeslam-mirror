9cdb75f8dd336b9754dbf03dcc66747d6273a933
Robert Sköld <robert@publicclass.se>
2012-11-01 14:06:08 +0100

    Use python instead of hardcoded python-2.5 in ./update*

--- update-goggles.sh
+++ update-goggles.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 sed -i.tmp 's/application: .*/application: gogglesgame/' app.yaml
-appcfg.py update .
+
+# use python instead of the python-2.5 that is hard coded in appcfg.
+# or ssl won't work on osx.
+python `which appcfg.py` update .
