699081a319fc38803672f361de43ad0529aa28c5
Einar Öberg <einar@northkingdom.com>
2012-11-05 16:39:35 +0100

    Added custom color as uiColor and renamed brightness to uiBrightness

--- public/javascript/shield_fs.glsl
+++ public/javascript/shield_fs.glsl
@@ -3,7 +3,8 @@ precision mediump float;
 #endif
 
 varying vec2 vUv;
-uniform float brightness; 
+uniform float uiBrightness; 
+uniform vec3 uiColor; 
 
 float max3(float a,float b,float c)
 {
@@ -22,22 +23,23 @@ float rect( vec2 p, vec2 b, float smooth )
 void main( void ) {
 
     vec2 pos = vec2(1.0-vUv.x, vUv.y);
-    float glow = 0.061;
+    float glow = 0.081;
     float d1 = rect(pos-vec2(0.05,1.0), vec2(0.025,1.0), glow); 
-    vec3 clr1 = vec3(1.0,1.0,0.0) *d1; 
+    vec3 clr1 = uiColor *d1; 
 
     
     float d2 = rect( pos-vec2(0.50,1.0), vec2(0.025,1.0), glow); 
-    vec3 clr2 = vec3(0.0,0.0,1.0) *d2; 
+    vec3 clr2 = uiColor *d2; 
 
     float d3 = rect( pos-vec2(0.8,1.0), vec2(0.025,1.0), glow); 
-    vec3 clr3 = vec3(1.0,0.0,0.0) * d3; 
+    vec3 clr3 = uiColor * d3; 
     float glassFactor = vUv.x*(1.0-vUv.y)*0.1;
     vec3 clr = clr1+clr2+clr3 + vec3(2.0)*glassFactor;
     
     
     float scaline = abs(sin(pos.y * 200.0 ))/10.0;
 
-    gl_FragColor = vec4( (clr+scaline)*(brightness), ((d1+d2+d3)*0.5 + glassFactor)*(vUv.y+0.7));
+    //gl_FragColor = vec4( (clr+scaline)*(uiBrightness), ((d1+d2+d3)*0.5 + glassFactor)*(vUv.y+0.7));
+    gl_FragColor = vec4( clr, ((d1+d2+d3)*0.5 + glassFactor)*(vUv.y+0.7));
 
 }
