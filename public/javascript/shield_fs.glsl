fd45195feb1436950771707416cae1d0f4f146d2
Einar Öberg <einar@northkingdom.com>
2012-11-06 14:13:53 +0100

    Updated shaders

--- public/javascript/shield_fs.glsl
+++ public/javascript/shield_fs.glsl
@@ -3,8 +3,9 @@ precision mediump float;
 #endif
 
 varying vec2 vUv;
-uniform float uiBrightness; 
-uniform vec3 uiColor; 
+uniform float uBrightness; 
+uniform vec3 uColor;
+uniform float uColumnValues[ 18 ];
 
 float max3(float a,float b,float c)
 {
@@ -22,24 +23,18 @@ float rect( vec2 p, vec2 b, float smooth )
 
 void main( void ) {
 
-    vec2 pos = vec2(1.0-vUv.x, vUv.y);
-    float glow = 0.081;
-    float d1 = rect(pos-vec2(0.05,1.0), vec2(0.025,1.0), glow); 
-    vec3 clr1 = uiColor *d1; 
+    vec2 pos = vec2(vUv.x-1.0/36.0, vUv.y);
+    float glow = 0.01;
+    float col = 0.0;
 
-    
-    float d2 = rect( pos-vec2(0.50,1.0), vec2(0.025,1.0), glow); 
-    vec3 clr2 = uiColor *d2; 
+    for( float i = 0.0;i < 18.0;i++)
+    {
+        col += rect(pos-vec2(1.0/18.0*i,1.0), vec2(1.0/36.0,uColumnValues[int(i)]), glow)*1.0; 
+    }
 
-    float d3 = rect( pos-vec2(0.8,1.0), vec2(0.025,1.0), glow); 
-    vec3 clr3 = uiColor * d3; 
-    float glassFactor = vUv.x*(1.0-vUv.y)*0.1;
-    vec3 clr = clr1+clr2+clr3 + vec3(2.0)*glassFactor;
-    
-    
-    float scaline = abs(sin(pos.y * 200.0 ))/10.0;
-
-    //gl_FragColor = vec4( (clr+scaline)*(uiBrightness), ((d1+d2+d3)*0.5 + glassFactor)*(vUv.y+0.7));
-    gl_FragColor = vec4( clr, ((d1+d2+d3)*0.5 + glassFactor)*(vUv.y+0.7));
+    float glassFactor = vUv.x*(1.0-vUv.y);
+    vec3 clr = uColor*col + uColor*glassFactor;
+ 
+    gl_FragColor = vec4( clr, (col*0.8 + glassFactor)*((1.0-vUv.y)));
 
 }
