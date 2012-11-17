026a1d57930631c3ed93429efb99715f0b31223e
Einar Öberg <einar@northkingdom.com>
2012-11-17 21:33:02 +0100

    Removed effect again, to slow, need to revisit this

--- lib/shaders/camera_fs.glsl
+++ lib/shaders/camera_fs.glsl
@@ -9,15 +9,15 @@ void main(void)
 {
     vec2 pos = vUv;
     pos.x = (1.0-pos.x);
+    vec3 color = texture2D(tCamera,pos).rgb; 
     
-    float col = 1.0;
+    /*float col = 1.0;
     float n = sin(1.0*4.0 + pos.y * 600.0);
     col *= 0.9 + 0.1 * sign(n) * floor(abs(n) + 0.98);
 
-    vec3 color = texture2D(tCamera,pos).rgb; 
-    float bw = (color.r + color.g + color.b )/3.0;
-    vec3 saturate = mix(color,vec3(bw),0.8);
-    gl_FragColor = vec4(saturate*col,1.0);
     
+    float bw = (color.r + color.g + color.b )/3.0;
+    vec3 saturate = mix(color,vec3(bw),0.8);*/
+    gl_FragColor = vec4(color,1.0);
 }
 
