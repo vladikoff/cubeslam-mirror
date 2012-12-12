9dc1f4fab39186a08e3a421c0b11f667eb5920cd
Antoine Lehurt <antoine@northkingdom.com>
2012-12-12 09:17:24 +0100

    Add config file for Sass/Compass.

---
+++ ui/config.rb
@@ -0,0 +1,19 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+if environment == :production
+  relative_assets = true
+  output_style = :compressed
+  line_comments = false
+  color_output = false
+else
+	output_style = :expanded
+  relative_assets = true
+	line_comments = true
+	color_output = true
+  asset_cache_buster :none
+  sass_options = { :debug_info => true }
+end
