60b228b66ba326b614c333ffc64ef3eebe89679f
Einar Öberg <einar@northkingdom.com>
2013-01-17 12:19:28 +0100

    Added a decal to the shields

--- lib/shaders/shield_fs.glsl
+++ lib/shaders/shield_fs.glsl
@@ -5,17 +5,17 @@ precision mediump float;
 varying vec2 vUv;
 uniform float uBrightness; 
 uniform vec3 uColor;
+uniform sampler2D tDecal;
 
 void main( void ) {
 
-     vec2 p = gl_FragCoord.xy / vec2(1000.0,550.0);
+    float refl = gl_FragCoord.x / 2000.0;
 
-    float glassFactor = p.x*(0.7-p.y)*0.4;
+    float glassFactor = refl*0.3; 
 
-    glassFactor += vUv.x*(0.7-vUv.y)*0.2;
- 
+    glassFactor += vUv.x*(0.2-vUv.y)*0.1;
 
+    gl_FragColor = vec4( vec3(1.0), mix(glassFactor+.2,2.0,texture2D(tDecal,vUv).r));
 
-    gl_FragColor = vec4( vec3(1.0), glassFactor+0.1);
 
 }
