ff3240c53a6f32bffebff0b9a0449d1769eefae9
Einar Öberg <einar@northkingdom.com>
2013-02-01 12:41:03 +0100

    New digits on arena, now as separate planes and not in floor shader

--- lib/shaders/arena_fs.glsl
+++ lib/shaders/arena_fs.glsl
@@ -4,10 +4,7 @@ precision mediump float;
 
 varying vec2 vUv;
 uniform sampler2D tGrid;
-uniform sampler2D tDigits;
 uniform vec2 scale; 
-uniform vec2 points; 
-uniform vec2 circlePosArray[10];
 varying vec3 vWorldPosition;
 uniform vec2 resolution;
 
@@ -23,28 +20,10 @@ void main(void)
     float color2 = smoothstep( vUv.y,vUv.y+0.007,0.508);
     color2 -= smoothstep(vUv.y,vUv.y+0.007,0.498);
 
-    if( vUv.y < 0.5 ) {
-        float y = 0.4 - (vUv.y*0.9);
-
-        float rows = floor(points.x/4.0);
-        float offset = points.x-rows*4.0;
-
-        color2 += texture2D(tDigits,vec2(vUv.x/4.0 + offset*0.25,  1.0-y/2.0 - rows*0.19 )).x;
-    }
-    else {
-        float invY = (0.5-(1.0-(vUv.y*0.90)));
-
-        float rows = floor(points.y/4.0);
-        float offset = points.y-rows*4.0;
-
-        color2 += texture2D(tDigits,vec2((1.0-vUv.x)/4.0 + offset*0.25,   1.0-invY/2.0 - rows*0.19  )).x;
-    }
-
     vec3 gridColor = vec3(color, color, color);
 
     vec3 centerColor = vec3(color2)*lineColor;
     
-
     gl_FragColor = vec4( (gridColor)*lineColor , step(gridColor.x,0.99) )*0.2 + vec4( centerColor , color2);
  
 }
