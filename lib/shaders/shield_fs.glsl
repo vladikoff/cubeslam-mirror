892a9c5b96306a797a0992e48dcef8c4d2a60d23
Einar Öberg <einar@northkingdom.com>
2013-02-05 14:31:48 +0100

    Some more shield effects

--- lib/shaders/shield_fs.glsl
+++ lib/shaders/shield_fs.glsl
@@ -14,6 +14,6 @@ void main( void ) {
 
     glassFactor += vUv.x*(0.2-vUv.y)*0.1;
 
-    gl_FragColor = vec4( vec3(1.0), glassFactor+.2);
+    gl_FragColor = vec4( vec3(1.0), glassFactor+.2+ uBrightness);
 
 }
