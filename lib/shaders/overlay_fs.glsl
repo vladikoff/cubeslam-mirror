62770e2548d1b6fffd1b629d0b33c953f2fadb33
Einar Öberg <einar@northkingdom.com>
2013-02-15 12:22:31 +0100

    Added mirror effect on key 'm'

--- lib/shaders/overlay_fs.glsl
+++ lib/shaders/overlay_fs.glsl
@@ -8,7 +8,8 @@ uniform sampler2D tTape;
 
 void main(void) {
     vec2 p = gl_FragCoord.xy / resolution.xy;
-    gl_FragColor = vec4(texture2D(tTape,vUv).rgb + vec3(0.3) * sin( p.y * resolution.y*3.14 ),1.0);
+    //gl_FragColor = vec4(texture2D(tTape,vUv).rgb + vec3(0.3) * sin( p.y * resolution.y*3.14 ),1.0);
+    gl_FragColor = vec4(texture2D(tTape,vUv).rgb ),1.0);
 
 
 }
